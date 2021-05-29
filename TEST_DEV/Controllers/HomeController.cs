1e4073be50859c572c52f0054c4639a42234eba3
Milton Bravo <miltonbravo96@gmail.com>
2021-05-28 20:52:26 -0500

    Proyecto en blanco

--- TEST_DEV/Controllers/HomeController.cs
+++ TEST_DEV/Controllers/HomeController.cs
@@ -10,20 +10,7 @@ namespace TEST_DEV.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
+            ViewBag.Title = "Index";
             return View();
         }
     }
