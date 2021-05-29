aeb08dc306a3ea75bf51031b54410a2b480a6643
Milton Bravo <miltonbravo96@gmail.com>
2021-05-29 13:26:24 -0500

    Modelo para login

---
+++ TEST_DEV/Requests/LoginRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using TEST_DEV.Helpers;
+
+namespace TEST_DEV.Requests
+{
+    public class LoginRequest
+    {
+        [Required(ErrorMessage = "El correo es requerido")]
+        [RegularExpression(RegexHelper.Correo, ErrorMessage = "No es un correo eletrónico válido")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "No es un correo eletrónico válido")]
+        public string Correo { get; set; }
+        [Required(ErrorMessage = "La contraseña es requerida")]        
+        [DataType(DataType.Password, ErrorMessage = "Contraseña no válida")]
+        public string Password { get; set; }
+    }
+}
