dfaeb382873deb1b5c1067f94ce25144fba26da3
Milton Bravo <miltonbravo96@gmail.com>
2021-05-29 13:26:41 -0500

    Helper para MD5

---
+++ TEST_DEV/Helpers/MD5Helper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace TEST_DEV.Helpers
+{
+    public class MD5Helper
+    {
+        public static string CalcularHash(string input)
+        {
+            StringBuilder hash = new StringBuilder();
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            byte[] bytes = md5.ComputeHash(new UTF8Encoding().GetBytes(input));
+            for (int i = 0;i < bytes.Length; i++)
+            {
+                hash.Append(bytes[i].ToString("x2"));
+            }
+            return hash.ToString();
+        }
+
+        public static bool Comprar(string hash, string input)
+        {
+            return CalcularHash(input) == hash;
+        }
+    }
+}
