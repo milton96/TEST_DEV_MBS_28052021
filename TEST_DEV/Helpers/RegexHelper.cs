3c839ecbe3a9f24b4dc8fe4d81b0934be579736c
Milton Bravo <miltonbravo96@gmail.com>
2021-05-29 12:31:54 -0500

    helper para expresiones regulares

---
+++ TEST_DEV/Helpers/RegexHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TEST_DEV.Helpers
+{
+    public class RegexHelper
+    {
+        public const string Correo = @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$";
+    }
+}
