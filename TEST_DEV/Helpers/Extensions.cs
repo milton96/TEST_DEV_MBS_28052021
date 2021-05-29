8707ac8310dc6767a6b0e03cd87b5845f2166656
Milton Bravo <miltonbravo96@gmail.com>
2021-05-29 12:32:12 -0500

    Extensiones

---
+++ TEST_DEV/Helpers/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace TEST_DEV.Helpers
+{
+    public static class Extensions
+    {
+        public static bool IsCorreo(this string value)
+        {
+            return new Regex(RegexHelper.Correo).IsMatch(value);
+        }
+
+        public static T GetValor<T>(this SqlDataReader reader, int index)
+        {
+            return reader.IsDBNull(index) ? default : (T)reader.GetValue(index);
+        }
+    }
+}
