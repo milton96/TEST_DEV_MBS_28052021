22424ecea83ad09b97e1fe07a96a94fe0c8132e7
Milton Bravo <miltonbravo96@gmail.com>
2021-05-30 14:45:07 -0500

    Filtros para validar sesión iniciada o terminada

---
+++ TEST_DEV/Handlers/SesionIniciadaAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TEST_DEV.Models;
+
+namespace TEST_DEV.Handlers
+{
+    public class SesionIniciadaAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            Usuario u = (Usuario)filterContext.HttpContext.Session[Usuario.USUARIO];
+            if (u == null)
+            {
+                filterContext.Result = new RedirectToRouteResult(
+                    new System.Web.Routing.RouteValueDictionary
+                    {
+                        { "Controller", "Home" },
+                        { "Action", "Index" }
+                    }
+                );
+            }
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
