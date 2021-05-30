ecdd0520d2c9018b4c0895a69497a3cdc2300a07
Milton Bravo <miltonbravo96@gmail.com>
2021-05-29 23:38:00 -0500

    expresiones regulares para rfc

--- TEST_DEV/Helpers/RegexHelper.cs
+++ TEST_DEV/Helpers/RegexHelper.cs
@@ -8,5 +8,7 @@ namespace TEST_DEV.Helpers
     public class RegexHelper
     {
         public const string Correo = @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$";
+        public const string RFC = @"^([A-ZÑ&]{3,4}) ?(?:- ?)?(\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])) ?(?:- ?)?([A-Z\d]{2})([A\d])$";
+        public const string RFCPersonaFisica = @"^([A-ZÑ&]{4}) ?(?:- ?)?(\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])) ?(?:- ?)?([A-Z\d]{2})([A\d])$";
     }
 }
