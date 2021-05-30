b1f262c86ab2e49bd407e0a02684a5b575035706
Milton Bravo <miltonbravo96@gmail.com>
2021-05-30 13:56:08 -0500

    login html

--- TEST_DEV/Requests/LoginRequest.cs
+++ TEST_DEV/Requests/LoginRequest.cs
@@ -12,9 +12,12 @@ namespace TEST_DEV.Requests
         [Required(ErrorMessage = "El correo es requerido")]
         [RegularExpression(RegexHelper.Correo, ErrorMessage = "No es un correo eletrónico válido")]
         [DataType(DataType.EmailAddress, ErrorMessage = "No es un correo eletrónico válido")]
+        [Display(Name = "Correo electrónico")]
         public string Correo { get; set; }
         [Required(ErrorMessage = "La contraseña es requerida")]        
         [DataType(DataType.Password, ErrorMessage = "Contraseña no válida")]
+        [Display(Name = "Contraseña")]
         public string Password { get; set; }
+        public string Error { get; set; }
     }
 }
