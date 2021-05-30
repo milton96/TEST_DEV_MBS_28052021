9dd7798e2bc2ec033896cebc41b51d3dfbc49ed1
Milton Bravo <miltonbravo96@gmail.com>
2021-05-30 02:00:02 -0500

    uikit y axios agregados

--- TEST_DEV/App_Start/BundleConfig.cs
+++ TEST_DEV/App_Start/BundleConfig.cs
@@ -9,10 +9,16 @@ namespace TEST_DEV
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/global").Include(
-                      "~/Scripts/site.js"));
+                "~/Scripts/uikit/uikit.min.js",
+                "~/Scripts/uikit/uikit-icons.min.js",
+                "~/Scripts/site.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/axios").Include(
+                "~/Scripts/axios/axios.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/site.css"));
+                "~/Content/uikit/uikit.min.css",
+                "~/Content/site.css"));
         }
     }
 }
--- TEST_DEV/Controllers/HomeController.cs
+++ TEST_DEV/Controllers/HomeController.cs
@@ -4,12 +4,21 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TEST_DEV.Models;
+using TEST_DEV.Requests;
 
 namespace TEST_DEV.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
+        {
+            ViewBag.Title = "Index";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(LoginRequest form)
         {
             ViewBag.Title = "Index";
             return View();
