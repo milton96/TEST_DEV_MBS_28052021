acd0e1c23d0285039100fffe8fa2ff0026503f5a
Milton Bravo <miltonbravo96@gmail.com>
2021-05-30 19:23:35 -0500

    creado controlador reportes

---
+++ TEST_DEV/Controllers/ReportesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TEST_DEV.Controllers
+{
+    public class ReportesController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Reportes";
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult ObtenerReportes()
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return null;
+        }
+    }
+}
---
+++ TEST_DEV/Scripts/reportes/reportes.js
@@ -0,0 +1,4 @@
+﻿UIkit.util.ready(function () {
+    const tabla = new Tabla("tabla-reportes", "/Reportes/ObtenerReportes");
+
+});
